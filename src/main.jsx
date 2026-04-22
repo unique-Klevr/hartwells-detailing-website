313b6800278760beb52d4e996eddc5cfca776ee8
Hammer <getmmg077@gmail.com>
2026-04-21 21:44:47 -0500

    Add Vite project setup for Vercel deployment

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import HartwellsDetailingWebsite from '../hartwells_detailing_website.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <HartwellsDetailingWebsite />
+  </React.StrictMode>,
+)
